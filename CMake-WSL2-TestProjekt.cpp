2bebecb6a0fd2881150918ff7c2b4a778aacfe49
Florian Gabriel <flow@p0cki.net>
2022-05-22 18:16:02 +0200

    Add project files.

---
+++ CMake-WSL2-TestProjekt.cpp
@@ -0,0 +1,13 @@
+﻿// CMake-WSL2-TestProjekt.cpp : Defines the entry point for the application.
+//
+
+#include "CMake-WSL2-TestProjekt.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	cout << "Hello Nanos." << endl;
+	return 0;
+}
